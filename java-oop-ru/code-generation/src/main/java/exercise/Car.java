11d15f7c9672fbbeec02666e357dcdf2794d18b1
@hexlet/cli <support@hexlet.io>
2024-04-19 14:02:18 +0700

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import lombok.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// BEGIN
+@Value
+// END
+ class Car {
+    int id;
+    String brand;
+    String model;
+    String color;
+    User owner;
+
+    // BEGIN
+    public String serialize() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRepresentation = objectMapper.writeValueAsString(this);
+        return jsonRepresentation;
+    }
+
+    public static Car unserialize(String jsonRepresentation) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonRepresentation, Car.class);
+    }
+}
